67a730c3a1d8b5e9211080f553d0910a0862ee29
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2017-11-05 16:26:02 +0100

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <p>Foo</p>
+        <div className='Book-form'>
+        </div>
+        <div className='Book-cover' />
       </div>
     );
   }

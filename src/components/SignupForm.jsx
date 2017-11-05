0dd624e559d6e756f832658ff24603566cecdaa9
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2017-11-05 17:39:46 +0100

    add signup form

---
+++ src/components/SignupForm.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import Button from 'material-ui/Button';
+import TextField from 'material-ui/TextField';
+import PropTypes from 'prop-types';
+
+const SignupForm = props => (
+  <form noValidate autoComplete='off' onSubmit={props.submitForm}>
+    <TextField
+      id='firstName'
+      label='First name'
+      value={props.firstName}
+      onChange={props.handleChange('firstName')}
+      margin='normal'
+      required
+    />
+    <TextField
+      id='lastName'
+      label='Last name'
+      value={props.lastName}
+      onChange={props.handleChange('lastName')}
+      margin='normal'
+      required
+    />
+    <TextField
+      id='email'
+      label='Email'
+      value={props.email}
+      onChange={props.handleChange('email')}
+      margin='normal'
+      fullWidth
+      required
+    />
+    <TextField
+      id='password'
+      label='Password'
+      value={props.password}
+      onChange={props.handleChange('password')}
+      type='password'
+      autoComplete='off'
+      margin='normal'
+      fullWidth
+      required
+    />
+    <div className='Button-container'>
+      {
+        props.checkFields() &&
+          <Button
+            color='contrast'
+            style={{ background: '#E52940' }}
+            type='submit'
+          >
+            Sign Up
+          </Button>
+      }
+      {
+        !props.checkFields() &&
+          <Button
+            color='contrast'
+            disabled
+          >
+            Sign Up
+          </Button>
+      }
+    </div>
+  </form>
+);
+
+SignupForm.propTypes = {
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  checkFields: PropTypes.func.isRequired,
+  submitForm: PropTypes.func.isRequired,
+};
+
+export default SignupForm;
--- src/containers/FormContainer.jsx
+++ src/containers/FormContainer.jsx
@@ -1,10 +1,48 @@
 import React from 'react';
+import Grid from 'material-ui/Grid';
+
+import ottoRadio from '../img/ottoradio.png';
+import SignupForm from '../components/SignupForm';
 
 export default class FormContainer extends React.Component {
+  state = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  }
+  handleChange = field => (event) => {
+    this.setState({
+      [field]: event.target.value,
+    });
+  }
+  checkFields = () => {
+    return this.state.email.length > 0 && this.state.firstName.length > 0 &&
+      this.state.lastName.length > 0 && this.state.password.length > 0;
+  }
+  submitForm = (e) => {
+    e.preventDefault();
+    console.log('Form submitted!');
+  }
   render() {
     return (
       <div className='Book-form'>
-        <p>Foo</p>
+        <img src={ottoRadio} alt='Otto Radio' />
+        <Grid container spacing={8} className='Form-container'>
+          <Grid item xs={12}>
+            <h1>Listen to City of Thieves free now.</h1>
+            <p>Try Otto Radio Unlimited free for 7 days.<br />Cancel any time.</p>
+            <SignupForm
+              firstName={this.state.firstName}
+              lastName={this.state.lastName}
+              email={this.state.email}
+              password={this.state.password}
+              handleChange={this.handleChange}
+              submitForm={this.submitForm}
+              checkFields={this.checkFields}
+            />
+          </Grid>
+        </Grid>
       </div>
     );
   }

b687baf1fa0fbdb3090a215e83fabd68f2a7c77e
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2017-11-05 13:23:17 +0100

    remove further boilerplate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <p>Foo</p>
       </div>
     );

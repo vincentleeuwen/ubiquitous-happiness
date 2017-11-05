55ea12cc6912dfcad43685f996f6a08666404ace
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2017-11-05 16:30:03 +0100

    refactor form to separate container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+
 import './App.css';
+import FormContainer from './containers/FormContainer';
+
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <div className='Book-form'>
-        </div>
+        <FormContainer />
         <div className='Book-cover' />
       </div>
     );
---
+++ src/containers/FormContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class FormContainer extends React.Component {
+  render() {
+    return (
+      <div className='Book-form'>
+        <p>Foo</p>
+      </div>
+    );
+  }
+}

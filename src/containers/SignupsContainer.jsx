5b7b4e7b43383f5d139d10422b10e9613effc564
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2017-11-05 21:35:29 +0100

    show signup data

--- src/containers/SignupsContainer.jsx
+++ src/containers/SignupsContainer.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import _ from 'lodash';
+import PropTypes from 'prop-types';
 
 export default class SignupsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signups: [],
+    };
+
+    const app = this.props.db.database().ref('signups');
+    app.on('value', (snapshot) => {
+      this.getData(snapshot.val());
+    });
+  }
+
+  getData(values) {
+    const signupsVal = values;
+    const signups = _(signupsVal)
+      .keys()
+      .map((signupKey) => {
+        const cloned = _.clone(signupsVal[signupKey]);
+        delete cloned.password;
+        return cloned;
+      })
+      .value();
+    this.setState({
+      signups,
+    });
+  }
   render() {
     return (
-      <div>Signups</div>
+      <pre>{JSON.stringify(this.state.signups)}</pre>
     );
   }
 }
+
+SignupsContainer.propTypes = {
+  db: PropTypes.objectOf({
+    database: PropTypes.func.isRequired,
+  }).isRequired,
+};
